44c939811265ffc118a963f99544ecef2484232c
Ilyasnow <Ilyasnow@i.ua>
2019-03-02 21:55:08 +0300

    Basic main file

---
+++ Abnormal-Main.py
@@ -0,0 +1,22 @@
+import discord
+
+client = discord.Client()
+
+print('Starting...')
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.channel.id != 477269419765006345:
+        return
+    
+    await message.channel.send(message.contents)
+
+client.run(TOKEN)
